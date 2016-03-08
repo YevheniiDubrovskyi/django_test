25b5f9ea636903d067b1bf7707502c9fce6d0acc
DubrovskiyEvgeniy <dubrovskiiz@mail.ru>
2016-03-08 21:38:30 +0200

    added virtual environment

--- init.sh
+++ init.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#sudo ln -sf /home/box/web/hello.py   /etc/gunicorn.d/hello.py
-#sudo ln -sf /home/box/web/ask/ask/wsgi.py   /etc/gunicorn.d/ask.py
