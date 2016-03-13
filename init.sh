1632d78e398b87626b8000edd49d996ff281a801
DubrovskiyEvgeniy <dubrovskiiz@mail.ru>
2016-03-13 18:53:36 +0200

    delete all to push again from another host

--- init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
--- web_venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/box/web/web_venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
