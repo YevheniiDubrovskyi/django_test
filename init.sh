45ad5cf312ddc041db855a4e1b4ba31fe8bf1f16
DubrovskiyEvgeniy <dubrovskiiz@mail.ru>
2016-03-16 11:47:43 +0200

    running on real machine

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+sudo ln -sf /home/dubrovskiy/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx stop
+sudo /etc/init.d/nginx start
--- web_venv/bin/django-admin.py
+++ web_venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/box/web/web_venv/bin/python
+#!/home/dubrovskiy/web/web_venv/bin/python
 from django.core import management
 
 if __name__ == "__main__":
