d9baa56cac663f990eb5b83936cf76154a593509
DubrovskiyEvgeniy <dubrovskiiz@mail.ru>
2016-03-19 21:41:14 +0200

    modified:   ask/ask/settings.py 	new file:   ask/qa/migrations/0003_auto_20160319_1317.py 	modified:   ask/qa/models.py 	modified:   ask/qa/urls.py 	modified:   ask/qa/views.py 	new file:   ask/templates/base.html 	new file:   ask/templates/new_questions.html 	new file:   ask/templates/popular_questions.html 	new file:   ask/templates/question.html 	modified:   etc/nginx.conf

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from qa import views
 
 urlpatterns = [
-	url(r'^$', views.test, name='root'),
-	url(r'^login/$', views.test, name='login'),
-	url(r'^signup/$', views.test, name='signup'),
-	url(r'^question/(?P<num>\d+)/$', views.test, name='question'),
-	url(r'^ask/$', views.test, name='ask'),
-	url(r'^popular/$', views.test, name='popular'),
-	url(r'^new/$', views.test, name='new'),
+    url(r'^$', views.new_questions, name='root'),
+    url(r'^login/$', views.test, name='login'),
+    url(r'^signup/$', views.test, name='signup'),
+    url(r'^question/(?P<id>\d+)/$', views.full_question, name='question'),
+    url(r'^ask/$', views.test, name='ask'),
+    url(r'^popular/$', views.popular_questions, name='popular'),
+    url(r'^new/$', views.test, name='new'),
 ]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_GET
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from models import *
+
+def paginate(request, qs, baseurl):
+    try:
+        limit = int(request.GET.get('limit', 10))
+    except ValueError:
+        limit = 10
+    if limit > 100:
+        limit = 10
+
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        raise Http404
+
+    paginator = Paginator(qs, limit)
+    paginator.baseurl = baseurl
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+
+    return page
 
 def test(request, *args, **kwards):
     return HttpResponse('OK')
+
+@require_GET
+def new_questions(request):
+    try:
+        questions = Question.objects.main()
+    except Question.DoesNotExist:
+        raise Http404
+    page = paginate(request, questions, '/?page=')
+    return render(request, 'new_questions.html', {
+        'questions': page.object_list,
+        'paginator': page.paginator,
+        'page': page,
+    })
+
+@require_GET
+def popular_questions(request):
+    try:
+        questions = Question.objects.popular()
+    except Question.DoesNotExist:
+        raise Http404
+    page = paginate(request, questions, '/popular/?page=')
+    return render(request, 'popular_questions.html', {
+        'questions': page.object_list,
+        'paginator': page.paginator,
+        'page': page,
+    })
+
+@require_GET
+def full_question(request, id):
+    qt = get_object_or_404(Questions, id=id)
+    return render(request, 'question.html', {
+        'question': qt,
+        'answers': qt.objects.get_answers(),
+    })
