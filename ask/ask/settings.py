80d417eddeefd05877a3bada936ed4ab6b564b3a
DubrovskiyEvgeniy <dubrovskiiz@mail.ru>
2016-03-21 20:54:54 +0200

    solved 400 error while Debug is False

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -24,8 +24,12 @@ SECRET_KEY = 'x)k-zsr)j^vd2))*d%qjytkbr)-i)(mtk8zi)898@y*ong!a0h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
