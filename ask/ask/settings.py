3abf8769b69828c6f3209168d381fb08c6d0ea31
DubrovskiyEvgeniy <dubrovskiiz@mail.ru>
2016-03-21 19:58:32 +0200

    add serving static files

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -128,6 +128,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR + '/qa/static',
+]
+
+STATIC_ROOT = BASE_DIR + '/static/'
+
 try:
     from ask.local_settings import *
 except ImportError:
